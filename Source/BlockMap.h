d55680c6aa151aa6f477b0b46c015ec44b206f36
AyameOtani <tky2502015@stu.o-hara.ac.jp>
2026-09-10 10:01:03 +0900

    ブロックマップの画像の導入

---
+++ Source/BlockMap.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "DxLib.h"
+// CSVを読み込んでブロックマップを生成するクラス
+
+// 1ブロックのサイズ 64
+const int CHIP_SIZZE = 64;
+
+// マップの横縦の最大チップ数
+const int MAX_MAP_WIDTH = 40;
+const int MAX_MAP_HEIGTHT = 20;
+
+// タイルセットの設定
+// 画像にチップが何個並んでいるか
+const int TILESET_COLUMNS = 5;
+
+class BlockMap
+{
+public:
+	BlockMap();
+	~BlockMap();
+
+	// CSVファイルと画像をセットで読み込む関数
+	bool Load(const char* csvPath, const char* texturePath);
+
+	// マップの描画
+	void Draw();
+
+	// プレイヤーの指定した四角がマップに当たっているかの処理をする関数
+	bool IsCollision()
+};
+
+
+
