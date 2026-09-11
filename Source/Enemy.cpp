3db32b3b84dc92135a237be7886baa172223fe16
田中 陽太 <tky2502048@stu.o-hara.ac.jp>
2026-09-11 15:13:53 +0900

    敵のアニメーション画像の表示

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -1,81 +1,45 @@
 #include "Enemy.h"
 #include "DxLib.h"
-#include "Master.h"
-#include "Scene.h"
-#include "ObjectManager.h"
-#include "Player.h"
 #include "Texture.h"
 
 Enemy::Enemy(VECTOR initPos)
-	: Object2D("Resource/Image/SampleSkeleton.png", initPos) 
 	: Object2D("Resource/Image/enemy_dragon_move.png", initPos)
 {
 	SetTag(Object2D::Enemy2D);
-	mnCurrentFrame = 0;
-	mnFrameTimer = 0;
-	mvPlayerDirection = VGet(0.0f, 0.0f, 0.0f);
-}
 
 	moveSpeed = 2.0f;
 	isDamaged = false;
 	damageTimer = 0;
+	mnCurrentFrame = 0;
+	mnFrameTimer = 0;
 }
 
 Enemy::~Enemy()
 {
-
 }
 
 void Enemy::Update()
 {
 	// 画面左から出現するので、右へ移動させる
-	bool moved = false;
 
-	auto pObj = Master::mpSceneManager->GetCurrentScene()->GetObjectManager()->GetObject2DByTag(Object2D::BattlePlayer2D);
-	if (pObj != nullptr)
-	{
-		Player* pPlayer = dynamic_cast<Player*>(pObj);
-		if (pPlayer == nullptr)
-		{
-			return;
-		}
-
-		VECTOR playerToEnemy = VSub(pPlayer->GetPosition(), mvPosition);
-		float distance = VSize(playerToEnemy);
-
-		if (distance <= SearchRange)
-		{
-			mvPlayerDirection.x = (playerToEnemy.x > 0.0f) ? 1.0f : -1.0f;
-			mvPosition.x += MoveSpeed * mvPlayerDirection.x;
-			moved = true;
-		}
-		else
-		{
-			mvPlayerDirection.x = 0.0f;
-			moved = false;
-		}
-	
 	mvPosition.x += moveSpeed;
-	}
 
 	if (isDamaged)
-	if (moved)
 	{
 		damageTimer--;
 		if (damageTimer <= 0)
-		mnFrameTimer++;
-
-		if (mnFrameTimer >= FRAME_INTERVAL)
 		{
 			isDamaged = false;
-			mnFrameTimer = 0;
-			mnCurrentFrame = (mnCurrentFrame + 1) % TOTAL_FRAMES;
 		}
 	}
-	else
+
+	// アニメーション更新
+	mnFrameTimer++;
+	if (mnFrameTimer >= FRAME_INTERVAL)
 	{
-		mnCurrentFrame = 0;
 		mnFrameTimer = 0;
+		mnCurrentFrame =
+		(mnCurrentFrame + 1) % TOTAL_FRAMES;
 	}
 
 	Object2D::Update();
@@ -83,28 +47,17 @@ void Enemy::Update()
 
 void Enemy::Draw()
 {
-	if (isDamaged)
 	if (mpTexture != nullptr)
 	{
-		SetDrawBright(255, 100, 100);
-	}
 		const int srcX = mnCurrentFrame * FRAME_WIDTH;
 
-	Object2D::Draw();
-		int srcY = 101;
+		int srcY = 335;
 
-	if (isDamaged)
-		if (mvPlayerDirection.x > 0.0f)
-	{
-		SetDrawBright(255, 255, 255);
-			srcY = 354;
-	}
-		if (mvPlayerDirection.x < 0.0f)
+		if (isDamaged)
 		{
-			srcY = 101;
-}
+			SetDrawBright(255, 100, 100);
+		}
 
-void Enemy::OnDamaged()
 		DrawRectGraph(
 			static_cast<int>(mvPosition.x - FRAME_WIDTH / 2),
 			static_cast<int>(mvPosition.y - FRAME_HEIGHT / 2),
@@ -116,12 +69,22 @@ void Enemy::OnDamaged()
 			true
 		);
 
+		if (isDamaged)
+		{
+			SetDrawBright(255, 255, 255);
+		}
+
 	}
 	else
+	{
+		Object2D::Draw();
+	}
+
+}
+
+void Enemy::OnDamaged()
 {
 	isDamaged = true;
 	damageTimer = 30; // 30フレーム赤く光る
-		Object2D::Draw();
-	}
-	
+
 }
--- Source/Enemy.h
+++ Source/Enemy.h
@@ -11,20 +11,19 @@ public:
 	virtual void Draw() override;
 
 	// 攻撃を受けた際に呼ばれる処理
-	
+
 	void OnDamaged();
 
 private:
 	float moveSpeed;
 	bool isDamaged; // ダメージを受けて赤くなっているか
-	
+
 	int damageTimer; // 赤くするフレーム時間
-	
-	VECTOR mvPlayerDirection;
+
 	int mnCurrentFrame;
 	int mnFrameTimer;
 	static const int FRAME_WIDTH = 256;
 	static const int FRAME_HEIGHT = 256;
 	static const int TOTAL_FRAMES = 4;
-	static const int FRAME_INTERVAL = 10;
+	static const int FRAME_INTERVAL = 9;
 };
